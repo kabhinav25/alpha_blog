17b71045772d7821c7f89b62d3e79090475460a3
romi <kmr_abhinav@yahoo.co.in>
2016-07-08 02:28:50 +0530

    update current user to be article creatot

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,7 @@ def destroy
 end
   def create
     @article=Article.new(article_params)
+    @article.user=current_user
    if @article.save
      flash[:success] = "article saved"
     redirect_to article_path(@article)
