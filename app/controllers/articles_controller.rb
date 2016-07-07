551cdfbc2a8c934a9abcc72ae3f9d04747b8a47a
romi <kmr_abhinav@yahoo.co.in>
2016-07-08 02:25:46 +0530

    complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,13 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit,:update,:destroy,:show]
+  before_action :require_user, except: [:index,:show]
+  before_action :require_same_user, only: [:edit,:update,:destroy]
+  def require_same_user
+if current_user!=@article.user
+  flash[:danger]="you can only edit & delete your own articles"
+  redirect_to root_path
+end
+  end
   def index
     @articles=Article.paginate(page: params[:page],per_page:2)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit,:update,:show]
+  before_action :require_same_user, only: [:edit,:update]
+  def require_same_user
+    if current_user!=@user
+      flash[:danger]="you can only edit  your own account"
+      redirect_to root_path
+      end
+    end
+  def set_user
+    @user=User.find(params[:id])
+  end
   def index
     @users=User.paginate(page: params[:page],per_page:2)
   end
@@ -15,14 +26,11 @@ def create
   end
 end
   def show
-    @user=User.find(params[:id])
     @user_articles=@user.articles.paginate(page: params[:page],per_page:2)
   end
   def edit
-    @user=User.find(params[:id])
   end
   def update
-@user=User.find(params[:id])
     if @user.update(user_params)
       flash[:success]="Account updated successfully"
       redirect_to articles_path
