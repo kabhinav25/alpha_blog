f3957bffe12cf4325881924487ec62d3b8e9f31d
romi <kmr_abhinav@yahoo.co.in>
2016-07-11 23:48:35 +0530

    add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article <ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories,through: :article_categories
 validates :title, presence: {message:"pehle se hai"},length:{ minimum:3, maximum:20 }
 validates :description, presence: true,length:{ minimum:3, maximum:20 }
 validates :user_id,presence: true
